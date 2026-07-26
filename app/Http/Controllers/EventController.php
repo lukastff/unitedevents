e12b2bbd9c9334a6cb10bb3e2730ca3956876ecd
lukastff <lucastavaresffr@gmail.com>
2026-07-26 08:39:35 -0300

    feat: add route to remove user's event participation

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -134,4 +134,15 @@ class EventController extends Controller
 
         return redirect("/dashboard")->with('msg', 'Sua presença foi confirmada no evento ' . $event->title);
     }
+
+    public function leaveEvent($id) {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipants()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect("/dashboard")->with('msg', 'Você saiu com sucesso do evento ' . $event->title);
+    }
 }
--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -66,7 +66,14 @@
                         <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                         <td>{{ count($event->users) }}</td>
                         <td>
-                            <a href="#">Sair do evento</a>
+                            <form action="/events/leave/{{ $event->id }}" method="POST">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit">
+                                    <ion-icon name="trash-outline">Sair do Evento</ion-icon>
+                                    Sair do evento
+                                </button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
