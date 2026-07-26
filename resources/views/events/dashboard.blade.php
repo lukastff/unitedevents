70f3a4d0bd31a60f8e51f14661a77b71ebe70707
lukastff <lucastavaresffr@gmail.com>
2026-07-26 06:35:50 -0300

    feat: add delete events

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -23,7 +23,20 @@
                             <td scropt="row">{{ $loop->index + 1 }}</td>
                             <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                             <td>0</td>
-                            <td><a href="#">Editar</a> <a href="#">Deletar</a></td>
+                            <td>
+                                <a href="#">
+                                    <ion-icon name="create-outline"></ion-icon>
+                                    Editar
+                                </a>
+                                <form action="/events/{{ $event->id }}" method="POST">
+                                    @csrf
+                                    @method("DELETE")
+                                    <button type="submit">
+                                        <ion-icon name="trash-outline"></ion-icon>
+                                        Deletar
+                                    </button>
+                                </form>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
